3238466de8306e197e287e80e63da55928b1f2c3
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-28 10:59:46 +0200

    lol

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
+import React, { useState } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import NoFeedback from './NoFeedback/NoFeedback';
 import css from './app.module.css'
 
+
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
+  const [feedbackCounter, setFeedbackCounter] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+//ghpages jest ble
+  const [showStatistics, setShowStatistics] = useState(false);
 
-  useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const handleAddContact = (name, number) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number
-    };
-
-    const isContactExists = contacts.some(
-      (contact) => contact.name.toLowerCase() === name.toLowerCase()
-    );
-
-    if (isContactExists) {
-      alert(`${name} is already in contacts.`);
-      return;
-    }
-
-    setContacts((prevContacts) => [...prevContacts, contact]);
+  const handleFeedback = (type) => {
+    setFeedbackCounter((prevCounts) => ({
+      ...prevCounts,
+      [type]: prevCounts[type] + 1
+    }));
+    setShowStatistics(true);
   };
-
-  const handleDeleteContact = (contactId) => {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => contact.id !== contactId)
-    );
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
 
   return (
     <div>
-      <h1 className={css.h1}>Phonebook</h1>
-      <ContactForm onAddContact={handleAddContact} />
-      <h2>Contacts</h2>
-      <Filter filter={filter} onFilterChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
-    </div>
+    <h1 className={css.header}>Please Leave Feedback</h1>
+    <FeedbackOptions handleFeedback={handleFeedback} />
+    {showStatistics ? (
+      <Statistics feedbackCounter={feedbackCounter} />
+    ) : (
+      <NoFeedback />
+    )}
+  </div>
   );
 };
 
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import css from './contactList.module.css'
-
+import css from './contactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
@@ -8,12 +7,13 @@ const ContactList = ({ contacts, onDeleteContact }) => {
       {contacts.map((contact) => (
         <li key={contact.id}>
           {contact.name}: {contact.number}
-          <button onClick={() => onDeleteContact(contact.id)} className={css.delete}>Delete</button>
+          <button onClick={() => onDeleteContact(contact.id)} className={css.delete}>
+            Delete
+          </button>
         </li>
       ))}
     </ul>
   );
 };
- 
 
 export default ContactList;
